21785705c0160f61347fa3788c9ab272db0af89f
Blair Hamilton <bhamilton@draftkings.com>
2016-07-19 14:16:15 -0400

    bummped version

--- deep-security-agent/metadata.rb
+++ deep-security-agent/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'aws@trendmicro.com'
 license          'All rights reserved'
 description      'Installs/Configures the Deep Security agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 supports 'ubuntu'
 supports 'redhat'

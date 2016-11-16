98675c9546fd5161c7b67cba1b8b49e63875cb93
Kate Lynn <klynn@paybyphone.com>
2016-11-16 14:42:58 -0800

    Simplified local file name selection and URL building

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,154 +17,120 @@ require 'tmpdir'
 agent = node['deep-security-agent']
 
 
-# Matrix of agents to downloaded based on OS
-agent_download_info = {
-	"amzn" => {
-		"fn" => "ds_agent.rpm",
-		32 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/amzn1/i386/",
-		64 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/amzn1/x86_64/",
-		},
-	"rhel_4" => {
-		"fn" => "ds_agent.rpm",
-		32 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/RedHat_2.6.9_22.EL_i686/i386/",
-		64 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/RedHat_2.6.9_34.EL_x86_64/x86_64/",
-		},
-	"rhel_5" => {
-		"fn" => "ds_agent.rpm",
-		32 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/RedHat_EL5/i386/",
-		64 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/RedHat_EL5/x86_64/",
-		},
-	"rhel_6" => {
-		"fn" => "ds_agent.rpm",
-		32 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/RedHat_EL6/i386/",
-		64 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/RedHat_EL6/x86_64/",
-		},
-	"rhel_7" => {
-		"fn" => "ds_agent.rpm",
-		64 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/RedHat_EL7/x86_64/",
-		},
-	"suse_10" => {
-		"fn" => "ds_agent.rpm",
-		32 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/SuSE_10/i386/",
-		64 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/SuSE_10/x86_64/",
-		},
-	"suse_11" => {
-		"fn" => "ds_agent.rpm",
-		32 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/SuSE_11/i386/",
-		64 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/SuSE_11/x86_64/",
-		},
-	"suse_12" => {
-		"fn" => "ds_agent.rpm",
-		64 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/SuSE_12/x86_64/",
-		},
-	"win" => {
-		"fn" => "ds_agent.msi",
-		32 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/Windows/i386/",
-		64 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/Windows/x86_64/",
-		},
-	"ubuntu_10" => {
-		"fn" => "ds_agent.deb",
-		64 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/Ubuntu_10.04/x86_64/",
-		},
-	"ubuntu_12" => {
-		"fn" => "ds_agent.deb",
-		64 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/Ubuntu_12.04/x86_64/",
-		},
-	"ubuntu_14" => {
-		"fn" => "ds_agent.deb",
-		64 => "https://#{agent[:dsm_agent_download_hostname]}/software/agent/Ubuntu_14.04/x86_64/",
-		},	
-	}
-
-
-# Determine the correct agent based on information reported by Ohai
-agent_download_key = "amzn"
-case node[:platform_family]
-when /rhel/
-	if node[:kernel][:release].include?("amzn")
-		agent_download_key = "amzn"
-	else
-		agent_download_key = "rhel_#{/(\d+)/.match(node[:platform_version])[0]}"
-	end
-when /suse/
-	agent_download_key = "suse_#{/(\d+)/.match(node[:platform_version])[0]}"
-when /debian/
-	# @TODO need to differentiate between Ubuntu and Debian proper
-	agent_download_key = "ubuntu_#{/(\d+)/.match(node[:platform_version])[0]}"	
-when /win/
-	agent_download_key = "win"
-else
-	# try the RHEL agent as it seems to be the most common
-	agent_download_key = "rhel_6"
-end
-
-
-# assume 64-bit but allow for 32-bit when detected
+# determine bitness, assume 64-bit but allow for 32-bit when detected
 bitness = 64
 if node['kernel']['machine'] =~ /32/
 	bitness = 32
 end
-Chef::Log.info "Selected #{agent_download_key}/#{bitness} as the agent download based on Ohai reporting: #{node[:platform_family]}, #{node[:platform_version]}, #{node[:kernel][:release]}}"
 
 
-# Get the URL to download from
-agent_download_url = "#{agent_download_info[agent_download_key][bitness]}"
-Chef::Log.info "URL: #{agent_download_url}"
 
+#construct URL
+# @TODO need to differentiate between Ubuntu and Debian proper
+url = "https://#{agent[:dsm_agent_download_hostname]}:443/software/agent"
+
+platform_major_version = node[:platform_version].split('.').first
+case node[:platform_family]
+
+  when 'rhel' #redhat and amazon
+		if node[:kernel][:release].include?('amzn')
+			url << '/amzn1'
+    else
+      if platform_major_version == '4'
+				url << "/RedHat_2.6.9_22.EL_#{bitness == 64 ? 'x86_64' : 'i686'}"
+      else
+				url << "/RedHat_EL#{platform_major_version}"
+      end
+    end
+
+  when 'suse'
+		url << "/SuSE_#{platform_major_version}"
+
+	when 'windows'
+		url << '/Windows'
+
+	when 'debian'
+		url << "/Ubuntu_#{platform_major_version}.04"
+
+	else
+		raise 'Unsupported platform family ' + node[:platform_family]
+end
+
+url << "#{bitness == 64 ? '/x86_64/' : '/i386/'}Z"
+
+
+
+#determine local file path
+case node[:platform_family]
+	when 'rhel', 'suse'
+		local_file_name = 'ds_agent.rpm'
+	when 'windows'
+		local_file_name = 'ds_agent.msi'
+	when 'debian'
+		local_file_name = 'ds_agent.deb'
+	else
+		raise 'Unsupported platform family ' + node[:platform_family]
+end
 
 #figure out local path setup
 tmp_path = ''
 if ENV.has_key?('TMPDIR')
-	tmp_path = ENV['TMPDIR']
+  tmp_path = ENV['TMPDIR']
 elsif tmp_path = ENV['TMP']
-	tmp_path = ENV['TMP']
+  tmp_path = ENV['TMP']
 elsif tmp_path = ENV['TEMP']
-	tmp_path = ENV['TEMP']
+  tmp_path = ENV['TEMP']
 else
-	tmp_path = Dir.tmpdir()
+  tmp_path = Dir.tmpdir()
 end
+
 
 Chef::Log.info "Tmp folder determined to be [#{tmp_path}]"
-local_file_path = "#{tmp_path}/#{agent_download_info[agent_download_key]['fn']}"
-if agent_download_key =~ /win/
-	local_file_path = "#{tmp_path}\\#{agent_download_info[agent_download_key]['fn']}"
+local_file_path = "#{tmp_path}/#{local_file_name}"
+if node[:platform_family] == 'windows'
+  local_file_path = "#{tmp_path}\\#{local_file_name}"
 end
 
-Chef::Log.info "Local file: #{local_file_path}"
 
 
 # Download the agent
+Chef::Log.info "downloading #{url} to #{local_file_path} as the based on Ohai reporting: #{node[:platform_family]}, #{node[:platform_version]}, #{node[:kernel][:release]}}"
 if agent[:ignore_ssl_validation]
 	open(local_file_path, 'wb') do |file|
-		request_uri=URI.parse(agent_download_url)
+		request_uri=URI.parse(url)
 		file << open(request_uri, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}).read
 	end
 else
 	remote_file local_file_path do
-	    source agent_download_url
+	    source url
 	    action :create
 	end
 end
 
 
-# Install the agent
-if agent_download_key =~ /ubuntu/
-	dpkg_package "ds_agent" do
-	  source local_file_path
-	  action :install
-	end
-elsif agent_download_key =~ /suse/
-	rpm_package "ds_agent" do
-	  source local_file_path
-	  action :install
-	end
-else
-	package "ds_agent" do
-	  source local_file_path
-	  action :install
-	end
+
+#install agent package
+case node[:platform_family]
+  when 'rhel', 'suse'
+    rpm_package "ds_agent" do
+      source local_file_path
+      action :install
+    end
+  when 'windows'
+    package "ds_agent" do
+      source local_file_path
+      action :install
+    end
+  when 'debian'
+    dpkg_package "ds_agent" do
+      source local_file_path
+      action :install
+    end
+  else
+    raise 'Unsupported platform family ' + node[:platform_family]
 end
 Chef::Log.info "ds_agent package installed successfully"
+
 
 
 # Make sure the service is running
@@ -179,6 +145,7 @@ rescue
 end
 
 
+
 # Block the wait to ensure it's sequential
 ruby_block 'metadata_wait' do
 	block do
@@ -186,6 +153,7 @@ ruby_block 'metadata_wait' do
 	end
 end
 Chef::Log.info "ds_agent package installed. ds_agent service is running. Ready to activate"
+
 
 
 # Activate the agent

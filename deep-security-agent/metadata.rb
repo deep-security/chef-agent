62c096e53df5bf9cf5959e14c83444462094608a
Yvon <yvon_liu@trendmicro.com>
2019-03-13 14:25:14 +0800

    DS-32726 detecting amazon linux 2

--- deep-security-agent/metadata.rb
+++ deep-security-agent/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'deepsecurityopensource@trendmicro.com'
 license          'All rights reserved'
 description      'Installs/Configures the Deep Security Agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.0'
+version          '1.2.1'
 
 supports 'amazon'
 supports 'centos'

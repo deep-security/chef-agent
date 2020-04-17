6c24cace531351c2191eabe4e4a8cd729d9466ae
nimesh-jethwa <nimesh.jethwa@live.com>
2020-04-17 20:40:49 +0000

    Add the required metadata for the cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,16 @@
 name             'workload-security-agent'
 maintainer       'Trend Micro'
 maintainer_email 'deepsecurityopensource@trendmicro.com'
-license          'All rights reserved'
+license          'Apache-2.0'
 description      'Installs/Configures the Workload Security Agent'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.0'
+version          '2.0.1'
+
+%w( amazon centos debian fedora oracle redhat ubuntu ).each do |os|
+  supports os
+end
+
+source_url 'https://github.com/deep-security/chef-agent'
+issues_url 'https://github.com/deep-security/chef-agent/issues'
+chef_version '>= 12.18'
 
 depends 'aws', '>= 8.2.0'
